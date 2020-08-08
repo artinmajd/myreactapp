666048693349ba26dd86b25b3a1e30cd906a1573
Artin Majd <artinmajd@Artins-MacBook-Pro.local>
2020-08-08 21:03:21 +0430

    The begining

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return(
+    <div>
+      <div className='background'>
+        <div className='page-title'>Projects so Far </div>
+        </div>
+      </div>
+
+  )
+}
+
+export default App;
