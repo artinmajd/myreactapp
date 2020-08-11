ac8e2e1765405b1bbaebc4edc795c7bde2575920
Artin Majd <artinmajd@Artins-MacBook-Pro.local>
2020-08-12 04:09:59 +0430

    add a callbacks to App.js

--- src/guessWords/containers/App.js
+++ src/guessWords/containers/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import Congrats from "../components/Congrats";
 import GuessedWords from "../components/GuessedWords";
 import Input from "../containers/Input";
@@ -6,47 +6,55 @@ import Input from "../containers/Input";
 function App() {
   const [success, setSuccess] = useState(false);
   const [secretWord, setSecretWord] = useState("");
-  const [guessedWords, setGuessedWords] = useState([]);   ////in dare chikar mikone? chejuri va koja liste guessedword ro taghir bedim?
-  const [matchcount,setMatchCount] = useState([]);
-  
+  const [guessedWords, setGuessedWords] = useState([]);
+  const [matchcount, setMatchCount] = useState([]);
+  const guesswordCallback = useCallback(
+    (guessedWord) => {
+      if (guessedWord == secretWord) {
+        setSuccess(true);
+      }
+      setGuessedWords([
+        ...guessedWords,
+        {
+          guessedWord: guessedWord,
+          letterMatchCount: getLetterMatchCount(guessedWord, secretWord),
+        },
+      ]);
+    },
+    [guessedWords, secretWord]
+  );
   useEffect(() => {
     setSecretWord("Letter");
   }, []);
-
 
   const getLetterMatchCount = (guessedWord, secretWord) => {
     let t = 0;
     const min = Math.min;
     for (let i = 0; i < 26; i++) {
-    t +=
-    min(
-    guessedWord.split(String.fromCharCode(i + 65)).length,
-    secretWord.split(String.fromCharCode(i + 65)).length
-    ) - 1;
+      t +=
+        min(
+          guessedWord.split(String.fromCharCode(i + 65)).length,
+          secretWord.split(String.fromCharCode(i + 65)).length
+        ) - 1;
     }
     for (let i = 0; i < 26; i++) {
-    t +=
-    min(
-    guessedWord.split(String.fromCharCode(i + 97)).length,
-    secretWord.split(String.fromCharCode(i + 97)).length
-    ) - 1;
+      t +=
+        min(
+          guessedWord.split(String.fromCharCode(i + 97)).length,
+          secretWord.split(String.fromCharCode(i + 97)).length
+        ) - 1;
     }
     return t;
-    };
+  };
 
-  const guessWord = (guessedWord) => {
-    if(guessedWord==secretWord){
-      setSuccess(true)
-    }
-    setGuessedWords([...guessedWords,{'guessedWord' : guessedWord, 'letterMatchCount' : getLetterMatchCount(guessedWord,secretWord) }]);
-  }
+  const guessWord = guesswordCallback;
 
   return (
     <div className={"container"}>
       <h2 className={"mt-2"}>Guess the word!</h2>
       <Congrats success={success} />
-      <Input onGuessWord={guessWord} success = {success}/>
-      <GuessedWords  id={"guessed-word"} guessedWords={guessedWords}/>
+      <Input onGuessWord={guessWord} success={success} />
+      <GuessedWords id={"guessed-word"} guessedWords={guessedWords} />
     </div>
   );
 }
--- src/guessWords/containers/Input.js
+++ src/guessWords/containers/Input.js
@@ -1,28 +1,32 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useCallback } from "react";
 
 function Input(props) {
   const [guessedWord, setGuessedWord] = useState("");
-
   const onSubmitGuessedWord = () => {
-  if(guessedWord.length !=0)
-   {props.onGuessWord(guessedWord);
-    setGuessedWord('');}
-  
+    if (guessedWord.length != 0) {
+      props.onGuessWord(guessedWord);
+      setGuessedWord("");
+    }
+
     return null;
   };
 
-  const contents = props.success ? null : (     
+  const contents = props.success ? null : (
     <Fragment>
       <input
         value={guessedWord}
         id={"word-guess"}
         placeholder={"enter guess"}
         className={"mb-2 mx-sm-3"}
-        onChange={(event)=>{
-          setGuessedWord(event.target.value)
-          }}
+        onChange={(event) => {
+          setGuessedWord(event.target.value);
+        }}
       />
-      <button className={"btn btn-primary mb-2"} id={"guess-word-button"} onClick={onSubmitGuessedWord} >
+      <button
+        className={"btn btn-primary mb-2"}
+        id={"guess-word-button"}
+        onClick={onSubmitGuessedWord}
+      >
         Submit
       </button>
     </Fragment>
