24a584c68eca59252eea98f630e8a7ab1b7b85cf
Artin Majd <artinmajd@Artins-MacBook-Pro.local>
2020-08-17 18:57:06 +0430

    Updates

---
+++ precache-manifest.479ff233a0d0c41cad8f52ec9f45aae1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb8db70c336bfc4c574cfc0631d714b4",
+    "url": "/myreactapp/index.html"
+  },
+  {
+    "revision": "5c376690dc34ffc8a24f",
+    "url": "/myreactapp/static/css/main.52d4f55f.chunk.css"
+  },
+  {
+    "revision": "284d26ad5ca5b96177f9",
+    "url": "/myreactapp/static/js/2.409a0d23.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/myreactapp/static/js/2.409a0d23.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5c376690dc34ffc8a24f",
+    "url": "/myreactapp/static/js/main.76622872.chunk.js"
+  },
+  {
+    "revision": "ca87cd5cff795670c3ac",
+    "url": "/myreactapp/static/js/runtime-main.6728cf5c.js"
+  }
+]);
